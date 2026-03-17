9c9397b4463f8cb26a2e45bce0cf89c511d03806
Divya <divyapatelmp24@gamil.com>
2026-03-17 10:45:01 +0530

    update cors

--- frontend/src/app/services/notes.ts
+++ frontend/src/app/services/notes.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class NotesService {
 
-  apiUrl = "http://studyresourcebackend.onrender.com/api/notes";
+  apiUrl = "https://studyresourcebackend.onrender.com/api/notes";
 
   constructor(private http: HttpClient){}
 
